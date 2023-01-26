6abe575eace76b008ae4feb02abfc7998c6c4110
developer idblfs <developer.idblfs@gmail.com>
2023-01-26 19:32:08 +0530

    card done

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const { render } = require('prettyjson')
+const data = require('./lib/data')
+
+const renderOpts = {
+    dashColor: 'cyan',
+    keysColor: 'blue',
+    stringColor: 'white'
+}
+
+module.exports = ({ json }) => json ? JSON.stringify(data) : render(data, renderOpts)
